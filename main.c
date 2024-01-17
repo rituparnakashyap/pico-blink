c5232b76d8e6832220e98f90c8a8fd17d36b4678
Rituparna Kashyap <rituparnakashyap@gmail.com>
2024-01-16 23:24:10 -0800

    Adding ability to debug with UART

--- main.c
+++ main.c
@@ -3,16 +3,22 @@
 
 int main() {
     
+    // Init standard input and output
     stdio_init_all();
     if(cyw43_arch_init()) {
-    	printf("Wi-Fi init failed");
-	return -1;
+    	puts("Wi-Fi init failed");
+	    return -1;
     }
 
+    // Turn the onboard led on and off.
     while (true) {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        puts("LED on");
         sleep_ms(1000);
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        puts("LED off");
         sleep_ms(1000);
     }
+
+    return 0;
 }
